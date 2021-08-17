a874e6d453d163077b7f091154fefb8654036071
LucasACES <lucasc.a01@outlook.com>
2021-08-17 11:04:47 -0300

    Structure organization

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PROJETO-NESS',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/Time-1-ADS/PROJETO-NESS.git',
+    license='',
+    author='Lucas Cesar',
+    author_email='',
+    description='CRM OpenSource'
+)
