99abf6ef62451e1c82fc3140ad0e7dd855fd1de3
mvarocha <marcus.varocha@gmail.com>
2021-10-06 19:46:22 -0300

    criação e níveis de pipeline

--- codes/codes/static/js/main_screen.js
+++ codes/codes/static/js/main_screen.js
@@ -3,7 +3,29 @@ var listaDataProposta = [];
 var listaValorProposta = [];
 
 function novaProposta() {
-  var bloco = document.getElementById("propostas");
+
+  var radioColuna = document.getElementsByName("coluna");
+  if (radioColuna[0].checked){
+    var colunaSelecionada = "novasPropostas"
+  } else if (radioColuna[1].checked){
+    var colunaSelecionada = "primeiroContato"
+  } else if (radioColuna[2].checked){
+    var colunaSelecionada = "propostaFechada"
+  } else if(radioColuna[3].checked){
+    var colunaSelecionada = "projetoEmAndamento"
+  }
+
+  var radioPrioridade = document.getElementsByName("prioridade");
+  if (radioPrioridade[0].checked){
+    var prioridade = "prioridadeAlta"
+  } else if (radioPrioridade[1].checked){
+    var prioridade = "prioridadeMedia"
+  } else if (radioPrioridade[2].checked){
+    var prioridade = "prioridadeBaixa"
+  }
+  console.log(prioridade)
+  
+  var bloco = document.getElementById(colunaSelecionada);
 
   var nomeProposta = document.getElementById("nomeProposta").value;
   var dataProposta = document.getElementById("dataProposta").value;
@@ -14,7 +36,7 @@ function novaProposta() {
   listaValorProposta.push(valorProposta);
   for (var i = (listaNomeProposta.length)-1; i < listaNomeProposta.length; i++) {
     bloco.innerHTML +=
-      "<div class='elementoProposta'><strong>" +
+      "<div class='elementoProposta " + prioridade + "'><strong>" +
       listaNomeProposta[i] +
       "</strong><br>Data da proposta: " +
       listaDataProposta[i] +
@@ -26,5 +48,28 @@ function novaProposta() {
   document.getElementById("nomeProposta").value = "";
   document.getElementById("dataProposta").value = "";
   document.getElementById("valorProposta").value = "";
+  var limparRadioColuna = document.getElementsByName("coluna");
+  for(var i=0;i<limparRadioColuna.length;i++){
+    limparRadioColuna[i].checked = false;
+  }
+  var limparRadioPrioridade = document.getElementsByName("prioridade");
+  for(var i=0;i<limparRadioPrioridade.length;i++){
+    limparRadioPrioridade[i].checked = false;
+  }
+}
 
+
+function formularioNovaProposta(){
+  var blocoFormulario = document.getElementById("formularioNovaProposta");
+  blocoFormulario.className = "fundo-formulario display-flex"
+}
+
+function cancelarFormulario() {
+  var blocoFormulario = document.getElementById("formularioNovaProposta");
+  blocoFormulario.className = "fundo-formulario display-none"
+  var limparFormulario = document.getElementsByTagName("input")
+  for (var i=7; i<limparFormulario.length; i++){
+    limparFormulario[i].value = ""
+  }
+  
 }
