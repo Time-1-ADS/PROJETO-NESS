028cacb97c2e07b56a4cd7078cb22d808b6f3ed2
mvarocha <marcus.varocha@gmail.com>
2021-11-20 18:07:57 -0300

    login responsivo e correções na navbar

--- crm/core/static/js/navbar.js
+++ crm/core/static/js/navbar.js
@@ -4,15 +4,23 @@ var homeElement = document.getElementById("home");
 var contactsElement = document.getElementById("contacts");
 var dashboardElement = document.getElementById("dashboard");
 var registrationElement = document.getElementById("registration");
+var leadsElement = document.getElementById("leads");
+var profileElement = document.getElementById("profile");
 
-if (pageTitle == "Main Screen"){
+if (pageTitle == "Main Screen" || pageTitle == "Proposta" || pageTitle == "Delete"){
     homeElement.className += " active";
 } else if (pageTitle == "Contacts"){
     contactsElement.className += " active";
 } else if (pageTitle == "Dashboard"){
     dashboardElement.className += " active";
-} else if (pageTitle == "Register"){
+} else if (pageTitle == "Register" || pageTitle == "Customer Register" || pageTitle == "Employee Register"){
     registrationElement.className += " active";
+}
+else if (pageTitle == "Leads"){
+    leadsElement.className += " active";
+}
+else if (pageTitle == "Profile"){
+    profileElement.className += " active";
 }
 
 function navbar(){
