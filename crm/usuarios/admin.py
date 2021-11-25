471d8de70dfdf1a5102ee98cd850ed022bdae378
LucasACES <lucasc.a01@outlook.com>
2021-11-25 09:25:00 -0300

    ajustes

--- crm/usuarios/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from core.models import Agent
-
-# Register your models here.
-
-admin.site.register(Agent)
--- crm/usuarios/forms.py
+++
@@ -1,9 +0,0 @@
-from core.models import Agent
-from django import forms
-
-
-
-class CadastrarUsuario(forms.ModelForm):
-    class Meta:
-        model = Agent
-        fields = ['username', 'first_name', 'last_name', 'email', 'password','cargo']
