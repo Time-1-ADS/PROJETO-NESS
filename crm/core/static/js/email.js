72dccf0469d724a6569f71368c9368f238c3880f
mvarocha <marcus.varocha@gmail.com>
2021-11-26 22:38:59 -0300

    inicio sistema de e-mail

--- crm/core/static/js/email.js
+++ crm/core/static/js/email.js
@@ -1,3 +1,28 @@
-function email(){
-    
+function escolherDestinatario() {
+    var popup = document.getElementById("popup")
+    var a = assuntoFunc()
+    var m = mensagemFunc()
+    document.getElementById("emailClinica").href += "?subject="+a+"&amp;body="+m
+    popup.className = "hidden"
+}
+
+function assuntoFunc() {
+    var assunto = document.getElementById("assunto").value
+    return assunto
+}
+function mensagemFunc() {
+    var mensagem = document.getElementById("mensagem").value
+    return mensagem
+}
+function cancelarEnvio() {
+    document.getElementById("assunto").value = ""
+    document.getElementById("mensagem").value = ""
+    document.getElementById("popup").className = "hidden"
+}
+function email() {
+    document.getElementById("popup").className = "popup-background"
+}
+function atualiza(){
+    document.location.reload(true)
+    console.log("ae")
 }
